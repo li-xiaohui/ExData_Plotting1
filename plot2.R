d6d2805b012fe8c24f106c21d2096ed7371e70ec
li-xiaohui <xiaohui.george@gmail.com>
2015-04-09 16:27:28 +0800

    initial commit

---
+++ plot2.R
@@ -0,0 +1,25 @@
+library(sqldf)
+library(data.table)
+library(lubridate)
+
+# Plot 2
+df1 <- fread(filename, na.strings='?')
+## Converting dates
+# subset again to use paste function
+use<- subset(df1, Date=="1/2/2007" | Date=="2/2/2007")
+use$datetime <- paste(use$Date, use$Time)
+use$datetime <- dmy_hms(use$datetime)
+use$Date <- dmy(use$Date)
+use$Time <- hms(use$Time)
+use$Global_active_power <- as.numeric(use$Global_active_power)
+use$Global_reactive_power <- as.numeric(use$Global_reactive_power)
+use$Voltage <- as.numeric(use$Voltage)
+use$Sub_metering_1 <- as.numeric(use$Sub_metering_1)
+use$Sub_metering_2 <- as.numeric(use$Sub_metering_2)
+use$Sub_metering_3 <- as.numeric(use$Sub_metering_3)
+
+png("plot2.png",width=480,height=480,units="px",bg="transparent")
+plot(use$datetime, use$Global_active_power,  type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
+dev.off()
